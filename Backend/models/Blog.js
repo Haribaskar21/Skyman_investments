e2d61e815e7f6a34cb8e3512ae1867f934fc2dcf
Haribaskar <baskarh54@gmail.com>
2025-05-23 23:27:33 +0530

    Add files via upload

---
+++ Backend/models/Blog.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+
+const contentSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  date: { type: Date, default: Date.now },
+  image: String
+});
+
+module.exports = mongoose.model('Blog', contentSchema);
